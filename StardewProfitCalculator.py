5e54adaa037b45a0c4f50a8961530f29dd45bdb8
Monica Ong <myo@cornell.edu>
2019-06-01 18:13:43 -0400

    Read growth & regrowth from data, add getCropData function, & rename folder

--- StardewProfitCalculator.py
+++ StardewProfitCalculator.py
@@ -21,7 +21,7 @@ def readData():
 	'''
 	Reads quality chance and crop profit data from csv file.
 	'''
-	cropQualityPath = dataFilePath + 'Quality Crop Frequency/'
+	cropQualityPath = dataFilePath + 'Quality/'
 
 	for fertilizer in fertilizers:
 		with open(cropQualityPath + fertilizer + '.csv', 'r') as csvfile:
@@ -37,7 +37,7 @@ def readData():
 				item['gold'] = float(row[3])
 				eval(fertilizer + 'QualityChance').append(item)
 
-	cropProfitPath = dataFilePath + 'Crop Profit/'
+	cropProfitPath = dataFilePath + 'Crop/'
 
 	for season in seasons:
 		with open(cropProfitPath + season + '.csv', 'r') as csvfile:
@@ -53,12 +53,14 @@ def readData():
 				item['gold'] = float(row[3])
 				item['cost'] = float(row[4])
 				item['location'] = string.capwords(row[5].strip().lower())
+				item['growth'] = int(row[6])
+				item['regrowth'] = int(row[7])
 
 				eval(season + 'CropProfit')[item_name] = item
 
 def getCropQualityChance(level=0, boost=0, fertilizer='none'):
 	'''
-	Get the chance of recieving each quality of crop depending on 
+	Get the chance of recieving each quality of crop depending on
 	farming level and fertilizer.
 
 	Parameters:
@@ -69,7 +71,7 @@ def getCropQualityChance(level=0, boost=0, fertilizer='none'):
 		fertilizer: string, optional
 			Fertilizer used on patch. One of 'none', 'basic', or 'quality'.
 			Default fertilizer 'none'.
-	
+
 	Return:
 		quality : list
 			Percentage chance of getting each quality of the item
@@ -84,11 +86,61 @@ def getCropQualityChance(level=0, boost=0, fertilizer='none'):
 
 	return quality
 
+def getCropData(season, crop):
+	'''
+	Get crop data from season and crop name
+
+	Parameters
+		season : string
+			Season in which crop is grown. One of 'spring', 'summer', 'fall', or 'winter'
+		crop : string
+			Name of crop to get expected profit.
+
+	Return:
+		cropData: string dict
+			Information about crop.
+
+			Keys include:
+			'regular': float
+				Sell price for regular crop
+
+			'silver': float
+				Sell price for silver crop
+
+			'gold': float
+				Sell price for gold crop
+
+			'cost': float
+				Minimum cost to purchase crop
+
+			'location': string
+				Location where can buy crop at minium cost (in caps)
+
+			'growth': int
+				Number of days it takes for crop to harvest
+
+			'regrowth': int
+				Number of days it takes for crop to regrow after first harvest
+
+	'''
+	assert season in seasons, "Invalid season."
+
+	crop = string.capwords(crop.strip().lower())
+	cropData = {}
+
+	try:
+		cropData = eval(season + 'CropProfit')[crop]
+
+		# TODO: n2 account for case where breaks = what does function return??
+		return cropData
+	except:
+		print("Invalid crop name.")
+
 # TODO: NEED TO FACTOR IN YIELD PER CROP
 def getExpectedProfit(season, crop, level=0, boost=0, fertilizer='none'):
 	'''
 	Calculate the expected profit per crop based on farming level and fertilizer used.
-	
+
 	Parameters:
 		season : string
 			Season in which crop is grown. One of 'spring', 'summer', 'fall', or 'winter'
@@ -101,27 +153,13 @@ def getExpectedProfit(season, crop, level=0, boost=0, fertilizer='none'):
 		fertilizer : string, optional
 			Fertilizer used on patch. One of 'none', 'basic', or 'quality'.
 			Default fertilizer 'none'.
-	
+
 	Return:
 		profit : float
 			Expected profit of given crop at given level
 	'''
-	assert season in seasons, "Invalid season."
-
-	crop = string.capwords(crop.strip().lower())
-	cropData = {}
-	try:
-		cropData = eval(season + 'CropProfit')[crop]
-	except:
-		print("Invalid crop name.")
-
-	assert level >= 0 and level <= 10, "Invalid farming level."
-	assert boost >= 0 and boost <= 3, "Invalid boost amount."
-	assert fertilizer == 'none' or fertilizer == 'basic' or fertilizer == 'quality', "Invalid fertilizer type."
-
-	totalLevel = level + boost
-
-	qualityData = eval(fertilizer + 'QualityChance')[totalLevel]
+	cropData = getCropData(season, crop)
+	qualityData = getCropQualityChance(level, boost, fertilizer)
 
 	profit = 0
 
@@ -136,14 +174,7 @@ def getCropCosts(season, crop):
 	'''
 	Get the cost of the seed for a crop.
 	'''
-	assert season in seasons, "Invalid season."
-
-	crop = string.capwords(crop.strip().lower())
-	cropData = {}
-	try:
-		cropData = eval(season + 'CropProfit')[crop]
-	except:
-		print("Invalid crop name.")
+	cropData = getCropData(season, crop)
 
 	cost = cropData['cost']
 
